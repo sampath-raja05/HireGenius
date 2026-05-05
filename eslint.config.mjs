1e7a2cb50ffffe2276b3fdcff900b12e2eb1a412
sampath-raja05 <sampathraja228@gmail.com>
2026-05-05 20:51:53 +0530

    fixed

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals.js";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
+  ...(Array.isArray(nextVitals) ? nextVitals : [nextVitals]),
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
